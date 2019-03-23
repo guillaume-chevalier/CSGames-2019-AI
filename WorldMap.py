ef6fed9a37084007f87bf8c82e4fe3ac3dcc738b
Benjamin Girard <benjamin.girard.protic@gmail.com>
2019-03-23 10:50:03 -0400

    WorldMap

---
+++ WorldMap.py
@@ -0,0 +1,12 @@
+class WorldMap.py:
+    def __init__(self, world_map_dictionary):
+        self.world_map_dictionary = world_map_dictionary
+
+    def possible_moves(self):
+        possible_moves_dict = {}
+
+        for card in self.world_map_dictonary[]:
+            pass
+
+
+        return possible_moves_dict
